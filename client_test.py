e88788ef372d63f54f2a8e2be17603913360c034
Dharmik Sompura <dharmiksompura1212@gmail.com>
2024-06-15 16:27:17 +0530

    task 1 changes commited

--- client_test.py
+++ client_test.py
@@ -1,6 +1,15 @@
 import unittest
 from client3 import getDataPoint
 
+def getDataPoint(quote):
+    """ Produce all the needed values to generate a datapoint """
+    """ ------------- Update this function ------------- """
+    stock = quote['stock']
+    bid_price = float(quote['top_bid']['price'])
+    ask_price = float(quote['top_ask']['price'])
+    price = (bid_price+ask_price)/2
+    return stock, bid_price, ask_price, price
+
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
     quotes = [
@@ -8,18 +17,25 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            self.assertEqual(stock, quote['stock'])
+            self.assertEqual(bid_price, quote['top_bid']['price'])
+            self.assertEqual(ask_price, quote['top_ask']['price'])
+            self.assertEqual(price, (quote['top_bid']['price'] + quote['top_ask']['price']) / 2)
+  
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
-
+    for quote in quotes:
+            bid_price = float(quote['top_bid']['price'])
+            ask_price = float(quote['top_ask']['price'])
+            result = bid_price > ask_price
+            expected = True
+            self.assertEqual(result, expected, f"Expected bid_price ({bid_price}) to be greater than ask_price ({ask_price}) for stock {quote['stock']}")
   """ ------------ Add more unit tests ------------ """
-
-
-
 if __name__ == '__main__':
     unittest.main()
